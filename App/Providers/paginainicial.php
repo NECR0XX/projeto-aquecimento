9f07d2910e6c3a9e8d9007f264a73c8f95558a88
KarinaMauro <karinadasilvamauro@gmail.com>
2024-09-04 11:42:16 -0300

    pagina principal

---
+++ App/Providers/paginainicial.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../Public/Css/paginainicial.css">
+</head>
+<body>
+    <div class="barra">
+        <div class="cadastro">
+            cadastro.com
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="crud1">
+            <a href="link-para-crud1.html" class="link-container">
+                <img src="../../Public/Images/esportes.png" class="imagem-esporte">
+            </a>
+        </div>
+        <div class="crud2">
+            <a href="link-para-crud2.html" class="link-container">
+                <img src="../../Public/Images/competidores.png" class="imagem-competidores">
+            </a>
+        </div>  
+    </div>
+
+    <div class="container2">
+        <div class="crud3">
+            <a href="link-para-crud3.html" class="link-container">
+                <img src="../../Public/Images/treinadores.png" class="imagem-treinadores">
+            </a>
+        </div>  
+        <div class="crud4">
+            <a href="link-para-crud4.html" class="link-container">
+                <img src="../../Public/Images/localidades.png" class="imagem-localidades">
+            </a>
+        </div>
+    </div>
+</body>
+</html>
