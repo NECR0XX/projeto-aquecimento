4d8cd96b40680a6f117a523d3b2f1a8cc800aae8
LaraPrando <laramoura070506@gmail.com>
2024-09-06 09:11:28 -0300

    cadastro css

--- Public/Sport/CadastroSport.php
+++ Public/Sport/CadastroSport.php
@@ -18,13 +18,19 @@ if (isset($_POST['name']) &&
 <html>
 <head>
     <title>Cadastrar Esporte</title>
+    <link rel="stylesheet" href="Resources/Css/cadastro.css">
 </head>
 <body>
+   
     <header>
-
+ 
     </header>
     <main>
+        
         <section>
+            <div class="container">
+    <div class="form-box">
+    <h1>cadastro.com</h1>
             <h2>Cadastrar Esporte</h2>
             <form method="post">
                 <label>
@@ -40,5 +46,12 @@ if (isset($_POST['name']) &&
             </form>
         </section>
     </main>
+</div>
+</div> 
 </body>
 </html>
+
+        
+           
+           
+           
